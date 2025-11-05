7deb44e48c4c5eed9c4248498f851671eaa384e6
emptytorch <emptytorch@proton.me>
2025-11-05 18:16:37 +0100

    disable arrow keys

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+vim.keymap.set({ "n", "v", "i" }, "<up>", "<nop>")
+vim.keymap.set({ "n", "v", "i" }, "<down>", "<nop>")
+vim.keymap.set({ "n", "v", "i" }, "<left>", "<nop>")
+vim.keymap.set({ "n", "v", "i" }, "<right>", "<nop>")
