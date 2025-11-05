8a2d533b35aa204d2072f561fe88c13449312bd1
emptytorch <emptytorch@proton.me>
2025-11-05 18:20:51 +0100

    create new file in cwd

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
+vim.keymap.set("n", "<leader>fN", ':e <C-R>=expand("%:p:h") . "/" <cr>', {
+  desc = "New file (cwd)",
+})
+
 vim.keymap.set({ "n", "v", "i" }, "<up>", "<nop>")
 vim.keymap.set({ "n", "v", "i" }, "<down>", "<nop>")
 vim.keymap.set({ "n", "v", "i" }, "<left>", "<nop>")
