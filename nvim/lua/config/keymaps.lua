ac7b78c450c04848acb2e6a1b77567a0055cef97
emptytorch <emptytorch@proton.me>
2025-11-05 18:22:47 +0100

    add visual paste that preserves register

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@ vim.keymap.set("n", "<leader>fN", ':e <C-R>=expand("%:p:h") . "/" <cr>', {
   desc = "New file (cwd)",
 })
 
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
 vim.keymap.set({ "n", "v", "i" }, "<up>", "<nop>")
 vim.keymap.set({ "n", "v", "i" }, "<down>", "<nop>")
 vim.keymap.set({ "n", "v", "i" }, "<left>", "<nop>")
