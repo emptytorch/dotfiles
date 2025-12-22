6728a8462c7a7a0f2cdd3ce11661df3688538da8
emptytorch <emptytorch@proton.me>
2025-12-22 09:51:12 +0100

    font size 14

--- doom/config.el
+++ doom/config.el
@@ -24,7 +24,7 @@
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 ;;
-(setq doom-font (font-spec :family "Hack Nerd Font Mono" :size 12))
+(setq doom-font (font-spec :family "Hack Nerd Font Mono" :size 14))
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
