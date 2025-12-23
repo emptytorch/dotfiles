7f8eafd4579b90769ca0ca7148144937a0e4dd98
emptytorch <emptytorch@proton.me>
2025-12-23 08:34:41 +0100

    no free space

--- doom/config.el
+++ doom/config.el
@@ -53,6 +53,8 @@
 ;; Ignore option as modifier on macOS
 (setq mac-option-modifier 'none)
 
+(setq dired-free-space nil)
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
